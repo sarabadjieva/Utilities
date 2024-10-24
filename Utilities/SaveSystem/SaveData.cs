3968e2ed374e87e757aa747bc3ccc47a996cc1cb
Stefani Arabadjieva <s.arabadjieva00@gmail.com>
2024-10-24 09:26:56 +0300

    Really small update

---
+++ Utilities/SaveSystem/SaveData.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace Utilities.SaveSystem
+{
+    [DataContract]
+    internal class SaveData : IExtensibleDataObject
+    {
+        public ExtensionDataObject? ExtensionData { get; set; }
+
+        public Type[] KnownTypes { get; }
+
+        public SaveData()
+        {
+            KnownTypes = GetType()
+                .GetProperties()
+                .Where(p => p.GetCustomAttributes(true).Any(attr => attr is DataMemberAttribute))
+                .Select(p => p.GetType())
+                .ToArray();
+        }
+    }
+}
---
+++ Utilities/SaveSystem/SaveSystem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Utilities.SaveSystem
+{
+    internal class SaveSystem
+    {
+        private const string SAVE_EXT = ".txt";
+        private readonly string _saveFile;
+
+        public SaveSystem()
+        {
+            _saveFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "save", SAVE_EXT);
+
+        }
+
+        public void Save(SaveData data)
+        {
+            FileStream fs = new(_saveFile, FileMode.OpenOrCreate);
+            XmlDictionaryWriter writer = XmlDictionaryWriter.CreateTextWriter(fs);
+
+            DataContractSerializer serializer = new(data.GetType(), knownTypes: data.KnownTypes);
+            serializer.WriteObject(writer, data);
+
+            writer.Close();
+            fs.Close();
+        }
+
+        public SaveData Load()
+        {
+            return null;
+        }
+    }
+}
